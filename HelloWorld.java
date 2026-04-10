73b29491a8772029168923fe3d2e456a6d0e2b2a
shailajags730 <shailajags730@gmail.com>
2026-04-10 11:05:02 +0530

    Update HelloWorld.java with a simple Hello World program

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,3 +1,9 @@
+/*
+ * HelloWorld.java
+ * 
+ * A simple Java program that prints "Hello World" to the console.
+ */
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
